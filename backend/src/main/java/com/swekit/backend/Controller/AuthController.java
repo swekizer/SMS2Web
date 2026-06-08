3c8aefc41b2ea6935faf94f4dafeb31595677c4c
swekizer <swekit7@outlook.com>
2026-06-08 14:57:44 +0530

    CORS Origin fix

--- backend/src/main/java/com/swekit/backend/Controller/AuthController.java
+++ backend/src/main/java/com/swekit/backend/Controller/AuthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://sms-2-web.vercel.app/")
+@CrossOrigin(origins = {"https://sms-2-web.vercel.app/", "https://sms2web.onrender.com/"})
 
 @RestController
 public class AuthController {
